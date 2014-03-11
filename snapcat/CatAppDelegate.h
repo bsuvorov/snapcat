24ddeaf63aff8ba3542dae0403bcdb02c0fffeaf
Boris Suvorov <boris@box.com>
2014-03-11 12:20:57 -0700

    Initial commit of the XCODE project

---
+++ snapcat/CatAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CatAppDelegate.h
+//  snapcat
+//
+//  Created by Boris Suvorov on 3/11/14.
+//  Copyright (c) 2014 SnapCatBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CatAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ snapcat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  snapcat
+//
+//  Created by Boris Suvorov on 3/11/14.
+//  Copyright (c) 2014 SnapCatBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CatAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CatAppDelegate class]));
+    }
+}
